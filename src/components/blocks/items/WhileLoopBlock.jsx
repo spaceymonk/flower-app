fb3f0034d475cd4be727263e8f67ec95e1b6c118
Berktug Ozkan <berktug.ozkan@obss.com.tr>
2022-03-28 22:28:07 +0300

    bugfix: while loop scaling factor

--- src/components/blocks/items/WhileLoopBlock.jsx
+++ src/components/blocks/items/WhileLoopBlock.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { faRepeat } from '@fortawesome/free-solid-svg-icons';
-import { Handle, Position } from 'react-flow-renderer';
+import { Handle, Position, useViewport } from 'react-flow-renderer';
 import T from '../../../services/MessageConstants';
 import { BlockNode } from '../common/BlockNode';
 import { BlockTypes } from '../../../services/createNode';
@@ -11,19 +11,27 @@ import useBlockService from '../../../hooks/service/useBlockService';
 export function NodeComponent(node) {
   const processed = node.data.text; //todo: handle special keywords by bolding them etc.
   const { updateNode } = useBlockService();
+  const { zoom } = useViewport();
+
+  const handleResizeStart = React.useCallback((e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }, []);
+  const handleResize = React.useCallback(
+    (event, { size }) => {
+      updateNode(node.id, size);
+    },
+    [node.id, updateNode]
+  );
 
   return (
     <ResizableBox
+      transformScale={zoom}
       width={node.data.width || 200}
       height={node.data.height || 200}
       minConstraints={[200, 200]}
-      onResizeStart={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-      }}
-      onResize={(event, { size }) => {
-        updateNode(node.id, size);
-      }}
+      onResizeStart={handleResizeStart}
+      onResize={handleResize}
     >
       <BlockNode className="node-container" node={node}>
         <Handle type="target" position={Position.Top} className="handle" />
