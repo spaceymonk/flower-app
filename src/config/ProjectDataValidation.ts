f42377b5fb1ed3bff852cd750bb8abff53405689
spaceymonk <b.kaan.ozkan@gmail.com>
2022-05-20 16:43:36 +0300

    refactor: validation schemas updated

--- src/config/ProjectDataValidation.ts
+++ src/config/ProjectDataValidation.ts
@@ -1,6 +1,5 @@
 import Joi from 'joi';
-import { MarkerType } from 'react-flow-renderer';
-import { GlowTypes, BlockTypes, EdgeTypes } from '../types';
+import { GlowTypes, BlockTypes } from '../types';
 
 const blockTypeList = [
   BlockTypes.DECISION_BLOCK,
@@ -11,8 +10,6 @@ const blockTypeList = [
   BlockTypes.STOP_BLOCK,
   BlockTypes.WHILE_LOOP_BLOCK,
 ];
-
-const edgeTypeList = [EdgeTypes.CUSTOM_EDGE];
 
 const glowTypeList = [GlowTypes.NONE, GlowTypes.NORMAL, GlowTypes.ERROR];
 
@@ -25,33 +22,27 @@ export const BlockSchema = Joi.object({
     x: Joi.number().required(),
     y: Joi.number().required(),
   }).required(),
+  text: Joi.string().allow('').required(),
+  glow: Joi.string()
+    .valid(...glowTypeList)
+    .required(),
+  name: Joi.string(),
+  parentNodeId: Joi.string().allow(null).required(),
   width: Joi.number().required(),
   height: Joi.number().required(),
-  data: Joi.object({
-    name: Joi.string(),
-    text: Joi.string().allow(''),
-    glow: Joi.string().valid(...glowTypeList),
-  }).required(),
-}).options({ allowUnknown: true });
+}).options({ allowUnknown: false });
 
 export const EdgeSchema = Joi.object({
   id: Joi.string().required(),
-  source: Joi.string().required(),
-  target: Joi.string().required(),
+  sourceId: Joi.string().required(),
+  targetId: Joi.string().required(),
   sourceHandle: Joi.string().allow(null).required(),
   targetHandle: Joi.string().allow(null).required(),
-  type: Joi.string()
-    .valid(...edgeTypeList)
-    .required(),
-  markerEnd: Joi.object({
-    type: Joi.string().valid(MarkerType.ArrowClosed).required(),
-    color: Joi.string().valid('#505050').required(),
-  }).required(),
-}).options({ allowUnknown: true });
+}).options({ allowUnknown: false });
 
 export const ProjectDataSchema = Joi.object({
   title: Joi.string().min(5).max(30).required(),
   inputParams: Joi.string().allow('').required(),
   blocks: Joi.array().items(BlockSchema).required(),
-  edges: Joi.array().items(EdgeSchema).required(),
+  connections: Joi.array().items(EdgeSchema).required(),
 });
