53b21c49936b24b2aff9a0ac85d1aeeb4fefcdc0
spaceymonk <b.kaan.ozkan@gmail.com>
2022-05-29 12:40:13 +0300

    improvment: project helper created, service generalized

---
+++ src/services/helpers/ProjectHelper.ts
@@ -0,0 +1,46 @@
+import FileSaver from 'file-saver';
+import { toast } from 'react-toastify';
+import LocalStorageManager from '../../config/LocalStorageManager';
+import { ProjectDataSchema } from '../../config/ProjectDataValidation';
+import { ProjectData } from '../../types';
+import { nameof, throwErrorIfNull } from '../../util/common';
+import { BlockCreateFactory } from './BlockHelper';
+
+export function save(pd: ProjectData): void {
+  window.localStorage.clear();
+  window.localStorage.setItem(nameof<ProjectData>('blocks'), JSON.stringify(pd.blocks));
+  window.localStorage.setItem(nameof<ProjectData>('connections'), JSON.stringify(pd.connections));
+  window.localStorage.setItem(nameof<ProjectData>('title'), pd.title);
+  window.localStorage.setItem(nameof<ProjectData>('inputParams'), pd.inputParams);
+  LocalStorageManager.refresh();
+}
+
+export function download(pd: ProjectData): void {
+  const blob = new Blob([JSON.stringify(pd, null, 2)], { type: 'application/json' });
+  FileSaver.saveAs(blob, pd.title + '.json');
+}
+
+export function open(file: Blob, onOpen?: (content: ProjectData) => void): void {
+  const fileReader = new FileReader();
+  const handleFileRead = () => {
+    try {
+      const content: ProjectData = JSON.parse(throwErrorIfNull(fileReader.result).toString());
+      const result = ProjectDataSchema.validate(content);
+      if (result.error) {
+        throw new Error(result.error.message);
+      }
+      if (onOpen) {
+        onOpen({
+          title: content.title,
+          blocks: content.blocks.map((b: any) => BlockCreateFactory.fromJSON(b)),
+          connections: content.connections,
+          inputParams: content.inputParams,
+        });
+      }
+    } catch (e: any) {
+      toast.error('Invalid project file: ' + e.message);
+    }
+  };
+  fileReader.onloadend = handleFileRead;
+  fileReader.readAsText(file);
+}
