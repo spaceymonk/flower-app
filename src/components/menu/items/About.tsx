a54c9c1c82d1cc5ba51c3440a67364dac594000f
spaceymonk <b.kaan.ozkan@gmail.com>
2022-05-30 21:32:01 +0300

    refactor: minor changes

--- src/components/menu/items/About.tsx
+++ src/components/menu/items/About.tsx
@@ -9,7 +9,7 @@ import PropTypes from 'prop-types';
 export function AboutModal({ show, onClose }: AboutModalProps) {
   return (
     <Modal show={show} size="lg" centered onHide={onClose} scrollable>
-      <Modal.Header closeButton>
+      <Modal.Header closeButton >
         <h4>
           <FontAwesomeIcon size="lg" className="me-2" icon={faStar} /> About
         </h4>
@@ -19,13 +19,13 @@ export function AboutModal({ show, onClose }: AboutModalProps) {
           <p className="lead">
             Flower App <small>ver: {process.env.REACT_APP_VERSION}</small>
           </p>
-          <p className="text-center mx-5">
-            Flower App is a simple app to help you organize your flowcharts. You can add, remove, edit, export and
-            simulate your flowchart diagrams.
+          <p className="text-center mx-sm-5">
+            Flower App is a simple app to help you organize your flowcharts. You can add, remove, edit, export and simulate your flowchart diagrams.
           </p>
-          <p className="text-muted text-center small">
-            This project developed by Berktuğ Kaan Özkan (<a href="http://spaceymonk.github.io/">@spaceymonk</a>). You
-            can find the source code on <a href="https://github.com/spaceymonk/flower-app">GitHub page</a>.
+          <p className="text-center mx-sm-5">
+            This project developed by Berktuğ Kaan Özkan (<a href="http://spaceymonk.github.io/">@spaceymonk</a>). You can find the source code on{' '}
+            <a href="https://github.com/spaceymonk/flower-app">GitHub page</a>.
+            FlowerApp icon made by <a href="https://instagram.com/i.tasted.purple">Savaş Şen</a>.
           </p>
         </Container>
       </Modal.Body>
@@ -44,7 +44,6 @@ AboutModal.propTypes = {
 };
 
 export interface AboutModalProps extends PropTypes.InferProps<typeof AboutModal.propTypes> {}
-
 
 export function AboutMenuItem() {
   const [show, toggleShow] = useToggle();
