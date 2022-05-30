10447f769f9a9b71476822bd1dd0786a7a859075
spaceymonk <b.kaan.ozkan@gmail.com>
2022-05-30 08:45:36 +0300

    improvement: virtual input params created

--- src/model/block/FunctionBlock.ts
+++ src/model/block/FunctionBlock.ts
@@ -1,6 +1,10 @@
 import { MutableRefObject } from 'react';
+import { parse, eval as evaluate } from 'expression-eval';
+import { extractValue } from '../../services/helpers/SimulationHelper';
 import { BlockTypes, Memory, Point2D, ProjectData } from '../../types';
 import { SimpleBlock } from '../SimpleBlock';
+
+const functionRegex = /^\((.*)\)\s*=>\s*(([a-zA-Z_][a-zA-Z0-9_]*))$/;
 
 class FunctionBlock extends SimpleBlock {
   private _subroutine: ProjectData | null;
@@ -11,6 +15,31 @@ class FunctionBlock extends SimpleBlock {
   }
 
   public override async eval(memoryRef: MutableRefObject<Memory>) {
+    if (this._subroutine === null) {
+      throw new Error('No subroutine defined');
+    }
+
+    const functionMatch = this._text.trim().match(functionRegex);
+    if (!functionMatch) {
+      throw new Error(`Invalid subroutine call: ${this._text}`);
+    }
+
+    let args = '';
+    if (functionMatch[1].trim().length !== 0) {
+      args = functionMatch[1]
+        .split(',') // todo: better split
+        .map((arg) => {
+          const ast = parse(arg.trim());
+          const value = evaluate(ast, memoryRef.current);
+          return extractValue(value);
+        })
+        .join('\n');
+    }
+    const retVal = functionMatch[2].trim();
+
+    console.log('args:\n', args);
+    console.log('retVal:', retVal);
+
     return null;
   }
 
--- src/services/helpers/SimulationHelper.ts
+++ src/services/helpers/SimulationHelper.ts
@@ -16,3 +16,16 @@ export const displayValue = (value: any): string => {
   }
   return '-';
 };
+
+export const extractValue = (value: any): string => {
+  if (typeof value === 'string') {
+    return '"' + value + '"';
+  }
+  if (typeof value === 'number' || typeof value === 'boolean' || value === null) {
+    return String(value);
+  }
+  if (Array.isArray(value)) {
+    return `[${value.map((v) => extractValue(v)).join(', ')}]`;
+  }
+  throw new Error(`Invalid variable: ${value}`);
+};
