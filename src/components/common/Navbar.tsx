8310ea33e5d9e798fb3a9d8b4c520330e3de39b4
spaceymonk <b.kaan.ozkan@gmail.com>
2022-06-06 16:14:11 +0300

    bugfix: public_url

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar({ children }: PropTypes.InferProps<typeof Navbar.propTypes>) {
   return (
     <BsNavbar id="navbar" bg="light" expand="lg" className="px-4 user-select-none">
       <BsNavbar.Brand className="d-flex align-items-center" style={{marginTop:'-10px'}}>
-        <img src={process.env.PUBLIC_URL + '/logo192.png'} alt="" width="45" height="45" className="d-inline-block align-center" />
+        <img src={process.env.PUBLIC_URL + '/logo512.png'} alt="" width="45" height="45" className="d-inline-block align-center" />
         <span className="lead mt-2">{T.app.name}</span>
       </BsNavbar.Brand>
       {children}
--- src/components/menu/items/HowTo.tsx
+++ src/components/menu/items/HowTo.tsx
@@ -5,7 +5,7 @@ import { NavDropdown } from 'react-bootstrap';
 export function HowToMenuItem() {
   return (
     <>
-      <NavDropdown.Item target="_blank" href="/how-to.html">
+      <NavDropdown.Item target="_blank" href={process.env.PUBLIC_URL + '/how-to.html'}>
         <FontAwesomeIcon size="sm" className="me-2" icon={faQuestionCircle} /> How to Use
       </NavDropdown.Item>
     </>
