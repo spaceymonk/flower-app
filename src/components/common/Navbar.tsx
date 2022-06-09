d6d50bd4feb77b0ccf089101a7cbf4a726319fbc
Berktug Ozkan <b.kaan.ozkan@gmail.com>
2022-06-09 17:56:07 +0300

    bugfix: fixes #10

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -5,9 +5,9 @@ import PropTypes from 'prop-types';
 function Navbar({ children }: PropTypes.InferProps<typeof Navbar.propTypes>) {
   return (
     <BsNavbar id="navbar" bg="light" expand="lg" className="px-4 user-select-none">
-      <BsNavbar.Brand className="d-flex align-items-center" style={{marginTop:'-10px'}}>
-        <img src={process.env.PUBLIC_URL + '/logo512.png'} alt="" width="45" height="45" className="d-inline-block align-center" />
-        <span className="lead mt-2">{T.app.name}</span>
+      <BsNavbar.Brand className="d-flex align-items-center border bg-white p-1 rounded rounded-4" >
+        <img src={process.env.PUBLIC_URL + '/logo512.png'} alt="app-icon" width="30" height="30" />
+        <span className="lead ms-2">{T.app.name}</span>
       </BsNavbar.Brand>
       {children}
     </BsNavbar>
