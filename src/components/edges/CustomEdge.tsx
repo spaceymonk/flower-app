8bf974ebb93b62ef6d0528e377a2559d7af7e8e1
Berktug Ozkan <b.kaan.ozkan@gmail.com>
2022-06-09 11:46:39 +0300

    bugfix: fixes #4 removing button

--- src/components/edges/CustomEdge.tsx
+++ src/components/edges/CustomEdge.tsx
@@ -1,74 +1,12 @@
-import React from 'react';
-import { Button, Tooltip } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRemove } from '@fortawesome/free-solid-svg-icons';
-import CustomOverlay from '../common/CustomOverlay';
-import { getEdgeCenter, getSmoothStepPath, MarkerType, Position } from 'react-flow-renderer';
+import { getSmoothStepPath, MarkerType, Position } from 'react-flow-renderer';
 import PropTypes from 'prop-types';
-import { useServiceContext } from '../../providers/ServiceProvider';
-import { DecisionBlockHandle } from '../../types';
 
-export default function CustomEdge({
-  id,
-  sourceX,
-  sourceY,
-  targetX,
-  targetY,
-  sourcePosition,
-  targetPosition,
-  sourceHandleId,
-  markerEnd,
-}: CustomEdgeProps) {
-  const foreignObjectSize = 80;
+export default function CustomEdge({ id, sourceX, sourceY, targetX, targetY, sourcePosition, targetPosition, markerEnd }: CustomEdgeProps) {
   const edgePath = getSmoothStepPath({ sourceX, sourceY, sourcePosition, targetX, targetY, targetPosition });
-  const [edgeCenterX, edgeCenterY] = getEdgeCenter({ sourceX, sourceY, targetX, targetY });
-  const { connectionRepository } = useServiceContext();
-  const [showBtn, toggleBtn] = React.useState(false);
-
-  const handleClick = (evt: React.MouseEvent, id: string) => {
-    evt.stopPropagation();
-    const edge = connectionRepository.findById(id).orElseThrow(new Error('Edge not found'));
-    connectionRepository.delete(edge);
-  };
-
-  let x = edgeCenterX - foreignObjectSize / 2;
-  let y = edgeCenterY - foreignObjectSize / 2;
-
-  if (sourceHandleId === DecisionBlockHandle.TRUE || sourceHandleId === DecisionBlockHandle.FALSE) {
-    x = targetX - foreignObjectSize / 2;
-    y = sourceY - foreignObjectSize / 2;
-  }
 
   return (
     <>
       <path id={id} className="react-flow__edge-path" d={edgePath} markerEnd={markerEnd} markerStart="url(" />
-      <foreignObject
-        width={foreignObjectSize}
-        height={foreignObjectSize}
-        x={x}
-        y={y}
-        className="customedge-foreignobject"
-        requiredExtensions="http://www.w3.org/1999/xhtml"
-      >
-        <div
-          className="btn-wrapper"
-          onMouseEnter={() => toggleBtn(true)}
-          onMouseLeave={() => toggleBtn(false)}
-          style={{ opacity: showBtn ? '1' : '0' }}
-        >
-          <CustomOverlay overlay={<Tooltip>Delete Connection</Tooltip>}>
-            <Button
-              variant="danger"
-              size="sm"
-              disabled={!showBtn}
-              className="rounded rounded-circle customedge-btn d-flex justify-content-center align-items-center"
-              onClick={(event) => handleClick(event, id)}
-            >
-              <FontAwesomeIcon icon={faRemove} size="xs" />
-            </Button>
-          </CustomOverlay>
-        </div>
-      </foreignObject>
     </>
   );
 }
