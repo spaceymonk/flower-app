a68b704f6643b7e266967d4605e18bcf361e66a4
spaceymonk <b.kaan.ozkan@gmail.com>
2022-06-26 17:30:01 +0300

    bugfix: fixes #11

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,21 +1,21 @@
 import T from '../../config/MessageConstants';
 import { Navbar as BsNavbar } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import React from 'react';
 
-function Navbar({ children }: PropTypes.InferProps<typeof Navbar.propTypes>) {
+const Navbar = React.forwardRef(function ({ children }: any, ref: any) {
+  Navbar.propTypes = {
+    children: PropTypes.node,
+  };
   return (
-    <BsNavbar id="navbar" bg="light" expand="lg" className="px-4 user-select-none">
-      <BsNavbar.Brand className="d-flex align-items-center border bg-white p-1 rounded rounded-4" >
+    <BsNavbar ref={ref} id="navbar" bg="light" expand="lg" className="px-4 user-select-none">
+      <BsNavbar.Brand className="d-flex align-items-center border bg-white p-1 rounded rounded-4">
         <img src={process.env.PUBLIC_URL + '/logo512.png'} alt="app-icon" width="30" height="30" />
         <span className="lead ms-2">{T.app.name}</span>
       </BsNavbar.Brand>
       {children}
     </BsNavbar>
   );
-}
-
-Navbar.propTypes = {
-  children: PropTypes.node,
-};
+});
 
 export default Navbar;
--- src/components/menu/items/HowTo.tsx
+++ src/components/menu/items/HowTo.tsx
@@ -1,11 +1,52 @@
-import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { faExternalLink, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { NavDropdown } from 'react-bootstrap';
+import { NavDropdown, Tooltip } from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
+import useToggle from '../../../hooks/useToggle';
+import PropTypes from 'prop-types';
+import CustomOverlay from '../../common/CustomOverlay';
+import html from '../../../config/HowToText';
+
+import './HowTo.css';
+
+export function HowToModal({ show, onClose }: HowToModalProps) {
+  return (
+    <Modal id="how-to-modal" show={show} size="lg" centered scrollable onHide={onClose}>
+      <Modal.Header closeButton>
+        <h4>
+          <FontAwesomeIcon size="lg" className="me-2" icon={faQuestionCircle} /> How To Use
+        </h4>
+      </Modal.Header>
+      <Modal.Body>
+        <iframe title="How To" width="100%" height="95%" src={'data:text/html;base64,' + html} />
+      </Modal.Body>
+      <Modal.Footer>
+        <CustomOverlay overlay={<Tooltip>Open in new tab</Tooltip>}>
+          <Button variant="info" className="me-auto" target="_blank" href={process.env.PUBLIC_URL + '/how-to.html'}>
+            <FontAwesomeIcon icon={faExternalLink} />
+          </Button>
+        </CustomOverlay>
+        <Button variant="primary" size="sm" onClick={onClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+HowToModal.propTypes = {
+  show: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export interface HowToModalProps extends PropTypes.InferProps<typeof HowToModal.propTypes> {}
 
 export function HowToMenuItem() {
+  const [show, toggleShow] = useToggle();
   return (
     <>
-      <NavDropdown.Item target="_blank" href={process.env.PUBLIC_URL + '/how-to.html'}>
+      <HowToModal show={show} onClose={toggleShow} />
+      <NavDropdown.Item onClick={toggleShow}>
         <FontAwesomeIcon size="sm" className="me-2" icon={faQuestionCircle} /> How to Use
       </NavDropdown.Item>
     </>
